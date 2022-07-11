88ceb6615d972148aefc1d6f17805bb72a252f9f
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-07-11 22:58:59 +0700

    fix flex box for input header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -10,26 +11,31 @@ const Header = () => {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                    <a href="/">
+                    <Link to="/">
                         <img
                             src="https://static.fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
                             alt="F8"
                         />
-                    </a>
+                    </Link>
                     <h4 className={cx('title-logo')}>Học Lập Trình Để Đi Làm</h4>
                 </div>
-                <div className={cx('search')}>
-                    <div className={cx('icon-search')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <div className={cx('abc')}>
+                    <div className={cx('search')}>
+                        <div className={cx('icon-search')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </div>
+                        <input
+                            placeholder="Tìm kiếm khóa học, bài viết, video..."
+                            spellCheck={false}
+                            className={cx('search-input')}
+                        />
+                        <button className={cx('close-search')}>
+                            <FontAwesomeIcon icon={faXmark} />
+                        </button>
                     </div>
-                    <input
-                        placeholder="Tìm kiếm khóa học, bài viết, video..."
-                        spellCheck={false}
-                        className={cx('search-input')}
-                    />
-                    <button className={cx('close-search')}>
-                        <FontAwesomeIcon icon={faXmark} />
-                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <button className={cx('button')}>Đăng nhập</button>
                 </div>
             </div>
         </header>
