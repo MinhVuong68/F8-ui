1bf4d17e5eea731aaea3a111331140a540933d1e
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-07-11 16:07:10 +0700

    logo f8 ui

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,24 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 const Header = () => {
-    return <header>Header</header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <a href="/">
+                        <img
+                            src="https://static.fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+                            alt="F8"
+                        />
+                    </a>
+                    <h4 className={cx('title-logo')}>Học Lập Trình Để Đi Làm</h4>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
