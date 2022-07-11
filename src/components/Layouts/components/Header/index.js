d782229eb2d8657ea072b9b7213637febf90a534
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-07-11 17:20:59 +0700

    input search ui

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
@@ -15,6 +17,19 @@ const Header = () => {
                         />
                     </a>
                     <h4 className={cx('title-logo')}>Học Lập Trình Để Đi Làm</h4>
+                </div>
+                <div className={cx('search')}>
+                    <div className={cx('icon-search')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </div>
+                    <input
+                        placeholder="Tìm kiếm khóa học, bài viết, video..."
+                        spellCheck={false}
+                        className={cx('search-input')}
+                    />
+                    <button className={cx('close-search')}>
+                        <FontAwesomeIcon icon={faXmark} />
+                    </button>
                 </div>
             </div>
         </header>
