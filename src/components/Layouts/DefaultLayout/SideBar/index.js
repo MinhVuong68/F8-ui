88bff442a022667e48b73710e9b12739bfe0fb15
nguyenminhvuong <minhvuong06082001@gmail.com>
2022-07-22 12:07:16 +0700

    open tag write blog and slideshow no transition

--- src/components/Layouts/DefaultLayout/SideBar/index.js
+++ src/components/Layouts/DefaultLayout/SideBar/index.js
@@ -1,16 +1,31 @@
 import classNames from 'classnames/bind';
 import { useState } from 'react';
-import { CreateIcon } from '~/icons';
+import Tippy from '@tippyjs/react/headless';
+
+import { CreateIcon, PenIcon } from '~/icons';
 import ItemSidebar from './ItemSidebar';
 import listItemSidebar from './itemsSidebar';
 import styles from './Sidebar.module.scss';
+import Wrapper from '~/components/Popper/Wrapper';
+import { Link } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
 const SideBar = () => {
     const [choose, setChoose] = useState('');
+    const [open, setOpen] = useState(false);
 
     const handleChoose = (id) => {
         setChoose(id);
+    };
+
+    console.log(open);
+
+    const handleOpenCreate = () => {
+        setOpen(!open);
+    };
+
+    const handleOutSide = () => {
+        setOpen(false);
     };
 
     console.log(choose);
@@ -18,9 +33,30 @@ const SideBar = () => {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('create-button')}>
-                    <CreateIcon className={cx('create-icon')} />
-                </div>
+                <Tippy
+                    placement="bottom-end"
+                    interactive={true}
+                    trigger="click"
+                    onClickOutside={handleOutSide}
+                    render={(attrs) => (
+                        <Wrapper>
+                            <div className={cx('box-create')} tabIndex="-1" {...attrs}>
+                                <Link to="./" className={cx('write-blog')}>
+                                    <PenIcon />
+                                    <span>Viết blog</span>
+                                </Link>
+                            </div>
+                        </Wrapper>
+                    )}
+                >
+                    <div className={cx('create-button')} onClick={handleOpenCreate}>
+                        <CreateIcon
+                            className={cx('create-icon', {
+                                open: open,
+                            })}
+                        />
+                    </div>
+                </Tippy>
 
                 <ul className={cx('list')}>
                     {listItemSidebar.map((item, index) => (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,23 @@
+import styles from './Home.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 const Home = () => {
-    return <h2>Home page</h2>;
+    return (
+        <div className={cx('slideshow')}>
+            <div className={cx('left')}>
+                <a href="/">F8 trên Facebook</a>
+                <p>
+                    F8 được nhắc tới ở mọi nơi, ở đâu có cơ hội việc làm cho nghề IT và có những con người yêu thích lập
+                    trình F8 sẽ ở đó.
+                </p>
+            </div>
+            <div className={cx('right')}>
+                <img src="https://files.fullstack.edu.vn/f8-prod/banners/Banner_04_2.png" alt="img"></img>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
